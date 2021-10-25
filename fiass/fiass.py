49de47a496c6b45eafd87bffd1d91772cf5d268f
avinashronanki <ronanki.avinash@gmail.com>
2021-10-25 15:24:46 +0200

    similarty search and index product quantization using fiass and SBERT

---
+++ fiass/fiass.py
@@ -0,0 +1,4 @@
+#https://towardsdatascience.com/getting-started-with-faiss-93e19e887a0c
+# https://www.pinecone.io/learn/semantic-search/
+#https://radimrehurek.com/gensim/auto_examples/tutorials/run_annoy.html
+#https://markroxor.github.io/gensim/static/notebooks/annoytutorial.html
